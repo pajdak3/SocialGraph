2bfed6bda25ef7f4db7b3ffd02e91f028e8ee4e2
pajdak3 <vik.pajdak@gmail.com>
2017-12-02 21:54:36 +0100

    base

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,27 +3,55 @@ var Application = function () {
     
     var app = {};
 
-    app.widgets = {
+    app.global = {
 
         init: function () {
             this.someFunction();
+            this.blazy();
         },
 
         someFunction: function () {
             console.log("happens");
-        }
-            
+        },
+
+        blazy: function () {
+            var bLazy = new Blazy({
+                offset: 200,
+                success: function (ele) {
+                    ele.classList.add("loaded");
+                }
+            });
+        }  
     };
 
     // Scroll tricker
     app.scroll = {
 
         init: function () {
-            this.scroller();
+            this.header();
         },
 
-        scroller: function () {
+        header: function () {
+            var header = document.querySelector('header');
+            var lastScrollTop = 0;
+            var hideHeight = 500;
 
+            window.onscroll = function () {
+                top_of_window = window.pageYOffset || document.documentElement.scrollTop;
+
+                // Hide and show scroller when scrolling down or up
+                if (window.scrollY > hideHeight) {
+            
+                    if (window.scrollY > lastScrollTop) {
+                        header.classList.add("hidden");
+        
+                    } else {
+                        header.classList.remove("hidden");
+                    }
+
+                    lastScrollTop = window.scrollY;
+                }
+            }
         }
 
     };
@@ -35,7 +63,8 @@ var Application = function () {
     // Initializer
     app.initialize = (function () {
 
-        app.widgets.init();
+        app.global.init();
+        app.scroll.init();
         // Globalize scope
         Application = app;
     }());
